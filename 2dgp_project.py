733deddca79b388ebdd973f69e1863c48164676b
jsw1211 <jswtiger@naver.com>
2023-11-12 17:50:21 +0900

    간단하게 바꿈

--- 2dgp_project.py
+++ 2dgp_project.py
@@ -1,51 +1,8 @@
-from pico2d import *
+from pico2d import open_canvas, delay, close_canvas
+import game_framework
 
-import game_world
-from background import BackGround
-from other_car import Other_Car
+import play_mode as start_mode
 
-
-def handle_event():
-    global running
-
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-    pass
-
-def create_world():
-    global running
-    global background
-    global other_car
-
-    running = True
-
-    other_car = Other_Car()
-    game_world.add_object(other_car, 1)
-    background = BackGround()
-    game_world.add_object(background, 0)
-    pass
-
-def update_world():
-    game_world.update()
-    pass
-
-def render_world():
-    clear_canvas()
-    game_world.render()
-    update_canvas()
-    pass
-
-open_canvas(840, 650)
-create_world()
-
-while running:
-    handle_event()
-    update_world()
-    render_world()
-    delay(0.01)
-
+open_canvas(840, 650, sync=True)
+game_framework.run(start_mode)
 close_canvas()
