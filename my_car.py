f6951b2dc9c3d7977fb1d61c37eb9c755e35bf8d
jsw1211 <jswtiger@naver.com>
2023-11-12 17:49:51 +0900

    내 차량 구현
    방향키 누르면 이동 하지만 좀 더 자연스러워야 할 것 같음

---
+++ my_car.py
@@ -0,0 +1,152 @@
+# 이것은 각 상태들을 객체로 구현한 것임.
+
+from pico2d import get_time, load_image, load_font, clamp,  SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_LEFT, SDLK_RIGHT
+import game_world
+import game_framework
+
+# state event check
+# ( state event type, event value )
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+
+PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30cm
+RUN_SPEED_KMPH = 20.0 # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+
+
+
+
+
+
+
+
+class Idle:
+
+    @staticmethod
+    def enter(my_car, e):
+        if my_car.face_dir == -1:
+            my_car.action = 2
+        elif my_car.face_dir == 1:
+            my_car.action = 3
+        my_car.dir = 0
+        my_car.frame = 0
+        pass
+
+    @staticmethod
+    def exit(my_car, e):
+        pass
+
+    @staticmethod
+    def do(my_car):
+        pass
+
+
+    @staticmethod
+    def draw(my_car):
+        my_car.image.clip_draw(385, 500, 80, 70, my_car.x, my_car.y, 100, 100)
+
+
+
+class Run:
+
+    @staticmethod
+    def enter(my_car, e):
+        if right_down(e) or left_up(e): # 오른쪽으로 RUN
+            my_car.dir, my_car.action, my_car.face_dir = 1, 1, 1
+        elif left_down(e) or right_up(e): # 왼쪽으로 RUN
+            my_car.dir, my_car.action, my_car.face_dir = -1, 0, -1
+
+    @staticmethod
+    def exit(my_car, e):
+        pass
+
+    @staticmethod
+    def do(my_car):
+        my_car.frame = (my_car.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        my_car.x += my_car.dir * RUN_SPEED_PPS * game_framework.frame_time
+        my_car.x = clamp(25, my_car.x, 1600 - 25)
+
+
+    @staticmethod
+    def draw(my_car):
+        my_car.image.clip_draw(385, 500, 80, 70, my_car.x, my_car.y, 100, 100)
+
+
+
+class StateMachine:
+    def __init__(self, my_car):
+        self.my_car = my_car
+        self.cur_state = Idle
+        self.transitions = {
+            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, space_down: Idle},
+            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, space_down: Run},
+        }
+
+    def start(self):
+        self.cur_state.enter(self.my_car, ('NONE', 0))
+
+    def update(self):
+        self.cur_state.do(self.my_car)
+
+    def handle_event(self, e):
+        for check_event, next_state in self.transitions[self.cur_state].items():
+            if check_event(e):
+                self.cur_state.exit(self.my_car, e)
+                self.cur_state = next_state
+                self.cur_state.enter(self.my_car, e)
+                return True
+
+        return False
+
+    def draw(self):
+        self.cur_state.draw(self.my_car)
+
+
+
+
+
+class My_Car:
+    def __init__(self):
+        self.x, self.y = 350, 90
+        self.frame = 0
+        self.action = 3
+        self.face_dir = 1
+        self.dir = 0
+        self.image = load_image('car.png')
+        self.state_machine = StateMachine(self)
+        self.state_machine.start()
+
+
+    def update(self):
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        self.state_machine.handle_event(('INPUT', event))
+
+    def draw(self):
+        self.state_machine.draw()
