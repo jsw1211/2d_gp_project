03d36a34e54ee625929d411e2e56bb89bc52f9f5
jsw1211 <jswtiger@naver.com>
2023-11-29 14:11:05 +0900

    배경이 움직이도록 구현

--- background.py
+++ background.py
@@ -1,9 +1,18 @@
 from pico2d import load_image
-
 class BackGround:
+    i = 0
+    j = 650
     def __init__(self):
         self.image = load_image('background.png')
     def draw(self):
-        self.image.draw(420, 325)
+        self.image.clip_draw_to_origin(0,0, 840, 650,0,self.i)
+        self.image.clip_draw_to_origin(0,0, 840, 650,0,self.j)
+        self.j-=1
+        self.i-=1
+        if self.i == -650:
+            self.i = 650
+        if self.j == -650:
+            self.j = 650
     def update(self):
         pass
+
